4ead851371029c7af5e74866dbe3185dd467ea10
TokcDK <tokcdk@gmail.com>
2020-07-03 09:28:30 +0500

    started extra settings realisation with standalone forms

---
+++ AI Girl Helper/Forms/ExtraSettings/Elements/XUnityAutotranslator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIHelper.Forms.ExtraSettings.Elements
+{
+    public partial class XUnityAutotranslator : Form
+    {
+        public XUnityAutotranslator()
+        {
+            InitializeComponent();
+        }
+    }
+}

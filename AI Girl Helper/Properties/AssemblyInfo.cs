18ed3ff511100f6eca035f4407f22770d0120cd2
TokcDK <tokcdk@gmail.com>
2021-03-16 20:33:50 +0500

    version up to 21.3.16.1

--- AI Girl Helper/Properties/AssemblyInfo.cs
+++ AI Girl Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.3.5.1")]
-[assembly: AssemblyFileVersion("21.3.5.2")]
+[assembly: AssemblyVersion("21.3.16.1")]
+[assembly: AssemblyFileVersion("21.3.16.1")]
 [assembly: NeutralResourcesLanguage("en")]

51c36a84536eaac4fba0a7bd41d7187bdc958c57
Denis K <tokcdk@gmail.com>
2021-06-04 12:39:42 +0500

    removed unused using

--- AI Girl Helper/Manage/ManageSettings.cs
+++ AI Girl Helper/Manage/ManageSettings.cs
@@ -3,7 +3,6 @@ using AI_Helper.Manage;
 using AIHelper.Games;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;

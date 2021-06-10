b2aaeb20ce807ceed6f1e8f317e335e37600719d
Denis K <tokcdk@gmail.com>
2021-06-10 18:45:37 +0500

    version raise for public link

--- AI Girl Helper/Properties/AssemblyInfo.cs
+++ AI Girl Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.5.22.1")]
-[assembly: AssemblyFileVersion("21.5.22.1")]
+[assembly: AssemblyVersion("21.6.10.1")]
+[assembly: AssemblyFileVersion("21.6.10.1")]
 [assembly: NeutralResourcesLanguage("en")]

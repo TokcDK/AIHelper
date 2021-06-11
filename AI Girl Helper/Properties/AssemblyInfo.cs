2104919903d12377dae002c955303a6e913874b3
Denis K <tokcdk@gmail.com>
2021-06-11 17:21:41 +0500

    version up to 2021.06.11.1

--- AI Girl Helper/Properties/AssemblyInfo.cs
+++ AI Girl Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.6.10.1")]
-[assembly: AssemblyFileVersion("21.6.10.1")]
+[assembly: AssemblyVersion("21.6.11.1")]
+[assembly: AssemblyFileVersion("21.6.11.1")]
 [assembly: NeutralResourcesLanguage("en")]

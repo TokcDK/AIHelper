429636cfff10588f3e9862560e94330a88f81b83
Denis K <tokcdk@gmail.com>
2021-09-01 22:58:34 +0500

    version up to 21.9.1.1

--- AI Helper/Properties/AssemblyInfo.cs
+++ AI Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.8.30.1")]
-[assembly: AssemblyFileVersion("21.8.30.1")]
+[assembly: AssemblyVersion("21.9.1.1")]
+[assembly: AssemblyFileVersion("21.9.1.1")]
 [assembly: NeutralResourcesLanguage("en")]

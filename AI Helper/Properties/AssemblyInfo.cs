b586f2e86c20098a30aacb993f6eedd043feb48e
Denis K <tokcdk@gmail.com>
2021-09-03 20:46:51 +0500

    version up to 21.9.3.1  , и снова третье сентября..

--- AI Helper/Properties/AssemblyInfo.cs
+++ AI Helper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("AI Helper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AI Helper - и снова третье сентября.. edition")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AI Helper")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.9.1.1")]
-[assembly: AssemblyFileVersion("21.9.1.1")]
+[assembly: AssemblyVersion("21.9.3.1")]
+[assembly: AssemblyFileVersion("21.9.3.1")]
 [assembly: NeutralResourcesLanguage("en")]

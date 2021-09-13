83b770f847b5fa0ee56b8c28ae410a561c737990
Denis K <tokcdk@gmail.com>
2021-09-13 16:14:26 +0500

    added com3d2 py basic game file for mo

---
+++ AI Helper/Other/basic_games/game_com3d2.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+from PyQt5.QtCore import QDir, QFileInfo, QStandardPaths
+
+import mobase
+
+from ..basic_game import BasicGame
+
+
+class IllusionKoikatuGame(BasicGame):
+    Name = "Custom order maid 3d2"
+    Author = "TokcDK<Denis K>"
+    Version = "0.1.0"
+
+    GameName = "Com3d2"
+    GameShortName = "com3d2"
+    GameNexusName = ""
+    GameNexusId = 0
+    GameSteamId = 0
+    GameBinary = "Data/COM3D2x64.exe"
+    GameDataPath = ""
+
+    def executables(self):
+        return [
+            mobase.ExecutableInfo(
+                "Com3d2",
+                QFileInfo(self.gameDirectory(), "Data/COM3D2x64.exe"),
+            )
+        ]
+
+    def dataDirectory(self):
+        return QDir(self._gamePath+"/Data")

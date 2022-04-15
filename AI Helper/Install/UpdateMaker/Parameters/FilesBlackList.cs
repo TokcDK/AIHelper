ec11c6e0a5d21ab43b4e5047be8e73af2aff435a
Denis K <tokcdk@gmail.com>
2022-04-15 20:15:57 +0500

    some trying to add simplier update maker parse

---
+++ AI Helper/Install/UpdateMaker/Parameters/FilesBlackList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIHelper.Install.UpdateMaker.Parameters
+{
+    internal class BlacklistDirs : IUpdateMakerIniParameter
+    {
+        public string Name => "BlacklistDirs";
+
+        public int Priority => 1000;
+
+        public string iniValue { get; set; }
+
+        public string Parse(string filePath)
+        {
+            foreach(var mask in iniValue.Split(','))
+            {
+            }
+
+            return filePath;
+        }
+    }
+}
---
+++ AI Helper/Install/UpdateMaker/Parameters/IUpdateMakerIniParameter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIHelper.Install.UpdateMaker.Parameters
+{
+    public interface IUpdateMakerIniParameter
+    {
+        /// <summary>
+        /// Parameter name
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Priority in parse order, bigger priority earlier Parse will be executed
+        /// </summary>
+        int Priority { get; }
+
+        /// <summary>
+        /// InitActions 
+        /// </summary>
+        string iniValue { get; set; }
+
+        /// <summary>
+        /// Parse file or dir path
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        string Parse(string filePath);
+    }
+}

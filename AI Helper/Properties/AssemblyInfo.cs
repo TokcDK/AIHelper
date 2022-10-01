becf61325e1d54a51b4796b4cd0ff88f5bf05d61
TokcDK <TokcDK@gmail.com>
2022-10-01 11:44:53 +0500

    version up to 22.10.1.1

--- AI Helper/Properties/AssemblyInfo.cs
+++ AI Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.09.30.1")]
-[assembly: AssemblyFileVersion("22.09.30.1")]
+[assembly: AssemblyVersion("22.10.1.1")]
+[assembly: AssemblyFileVersion("22.10.1.1")]
 [assembly: NeutralResourcesLanguage("en")]

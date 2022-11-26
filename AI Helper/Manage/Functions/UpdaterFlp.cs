c8ee809bbba3aff8466c33b1cfa7efb075f7ca22
TokcDK <TokcDK@gmail.com>
2022-11-26 21:38:22 +0500

    add updater fo flp

---
+++ AI Helper/Manage/Functions/UpdaterFlp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIHelper.Manage.Functions
+{
+    internal class UpdaterFlp : IFunctionForFlp
+    {
+        public string Symbol => T._("🌐");
+
+        public string Description => T._("Check for updates");
+
+        public void OnClick(object o, EventArgs e)
+        {
+            ManageUpdateMods.UpdateMods();
+        }
+    }
+}

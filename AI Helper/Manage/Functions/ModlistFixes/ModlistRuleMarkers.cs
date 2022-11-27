d5ceed33268c656cdf2c1e5c6fa2b43f9e11f714
TokcDK <TokcDK@gmail.com>
2022-11-27 21:00:03 +0500

    add static tags properties

---
+++ AI Helper/Manage/Functions/ModlistFixes/ModlistRuleMarkers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIHelper.Manage.Functions.ModlistFixes
+{
+    internal class ModlistFixesRuleTags
+    {
+        /// <summary>
+        /// The splitter between rule records means must be apply any part of the rule
+        /// </summary>
+        internal static string Or { get; } = "|or|";
+        /// <summary>
+        /// The splitter between rule records means must be apply all parts of the rule
+        /// </summary>
+        internal static string And { get; } = "|and|";
+        /// <summary>
+        /// The prefix means search file
+        /// </summary>
+        internal static string File { get; } = "file:";
+        /// <summary>
+        /// The search prefix means Required specified Mod dir name or File subpath in mod
+        /// </summary>
+        internal static string Req { get; } = "req:";
+        /// <summary>
+        /// The search prefix means Incompatible with specified Mod dir name or File subpath in mod
+        /// </summary>
+        internal static string Inc { get; } = "inc:";
+    }
+}

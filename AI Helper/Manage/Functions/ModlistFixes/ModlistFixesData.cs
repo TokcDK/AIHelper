264709855feb3f19bd003dbb8dd5ab9b6229a162
TokcDK <TokcDK@gmail.com>
2022-11-27 21:37:12 +0500

    add modlistfixes data for using by parsers

---
+++ AI Helper/Manage/Functions/ModlistFixes/ModlistFixesData.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using AIHelper.Data.Modlist;
+
+namespace AIHelper.Manage.Functions.ModlistFixes
+{
+    internal class ModlistFixesData
+    {
+        public ModlistFixesData(ModData mod)
+        {
+            Mod = mod;
+        }
+
+        /// <summary>
+        /// Currently parsing mod data
+        /// </summary>
+        internal ModData Mod { get; }
+
+
+        internal List<string> Messages = new List<string>();
+    }
+}

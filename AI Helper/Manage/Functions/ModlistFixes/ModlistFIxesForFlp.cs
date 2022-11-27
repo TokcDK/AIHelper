3ac5531ca7977ac1376367ff56ee6353c3f5ff3d
TokcDK <TokcDK@gmail.com>
2022-11-27 22:03:19 +0500

    add modlistfixes flp function

---
+++ AI Helper/Manage/Functions/ModlistFixes/ModlistFIxesForFlp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIHelper.Manage.Functions.ModlistFixes
+{
+    internal class ModlistFIxesForFlp : IFunctionForFlp
+    {
+        public string Symbol => "F";
+
+        public string Description => T._("Autodetect and fix issues in the current profile's modlist");
+
+        public void OnClick(object o, EventArgs e)
+        {
+        }
+    }
+}

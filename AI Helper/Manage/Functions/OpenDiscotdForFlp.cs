4c467f092e8e40d3120ee921417e7e99e40576d5
TokcDK <TokcDK@gmail.com>
2022-11-30 17:11:31 +0500

    add open discord as flp function

---
+++ AI Helper/Manage/Functions/OpenDiscotdForFlp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetListOfSubClasses;
+using System.Windows.Forms;
+using AIHelper.Games;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace AIHelper.Manage.Functions
+{
+    internal class OpenDiscotdForFlp : FunctionForFlpBase
+    {
+        public override string Symbol => T._("🎮");
+
+        public override string Description => string.Format(T._("Open 18+ Discord server of {0}", Application.ProductName));
+
+        public override void OnClick(object o, EventArgs e)
+        {
+            Process.Start(ManageSettings.DiscordGroupLink);
+        }
+    }
+}

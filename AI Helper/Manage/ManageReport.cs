1f2ccfe1a0896bbb52f9bac4815e2e9b741bfb79
TokcDK <TokcDK@gmail.com>
2022-11-30 22:45:51 +0500

    add some code to get ini data

--- AI Helper/Manage/ManageReport.cs
+++ AI Helper/Manage/ManageReport.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AIHelper.Manage
@@ -47,85 +46,179 @@ namespace AIHelper.Manage
                 }
             }
 
+            var langID = "." + ManageSettings.LanuageID.ToLowerInvariant();
+            var gameID = "." + ManageSettings.CurrentGame.GameAbbreviation.ToLowerInvariant();
 
-            var groupNames = new Dictionary<string, string>();
+            // build records
+            var dataForAdd = new Dictionary<string, LinkData>();
+            foreach (var info in iniInfos)
+            {
+                var data = info;
+
+                var id = data.Key;
+
+                if (!id.EndsWith(gameID)
+                    && !id.EndsWith(gameID + langID)
+                    && !id.EndsWith(".all")
+                    && !id.EndsWith($".all{langID}")
+                    ) continue;
+
+
+                var isLocaleBased = id.EndsWith(langID);
+
+                if (isLocaleBased)
+                {
+                    var nonLocaleId = id.Remove(id.Length - langID.Length);
+                    if (dataForAdd.ContainsKey(nonLocaleId)) dataForAdd.Remove(nonLocaleId);
+                }
+
+                var record = data.Value;
+
+                var @base = record.ContainsKey("base") ? record["base"] : "";
+
+                var link = record.ContainsKey("link") ? record["link"] : "";
+                link = RestoreString(iniInfos, link, "link", @base);
+                if (string.IsNullOrWhiteSpace(link)) continue;
+
+                var name = record.ContainsKey("name") ? record["name"] : "";
+            }
+
+            #region old
+            //var groupNames = new Dictionary<string, string>();
 
 
             //var langID = "<" + ManageSettings.LanuageID + ">";
 
-            string gameLinksPath = ManageSettings.LinksInfoFilePath;
-            if (string.IsNullOrWhiteSpace(gameLinksPath)) return;
+            //string gameLinksPath = ManageSettings.LinksInfoFilePath;
+            //if (string.IsNullOrWhiteSpace(gameLinksPath)) return;
 
-            string[] strings = File.ReadAllLines(gameLinksPath).Where(line => line.StartsWith(";##", StringComparison.InvariantCulture)).ToArray();
-            foreach (var line in strings)
+            //string[] strings = File.ReadAllLines(gameLinksPath).Where(line => line.StartsWith(";##", StringComparison.InvariantCulture)).ToArray();
+            //foreach (var line in strings)
+            //{
+            //    var en = Regex.Match(line, ";##([^<]+)");
+            //    if (!en.Success) continue;
+
+            //    var t = Regex.Match(line, langID + "([^<]+)");
+            //    if (!t.Success) continue;
+            //    if (groupNames.ContainsKey(en.Groups[1].Value)) continue;
+
+            //    groupNames.Add(en.Groups[1].Value, t.Groups[1].Value);
+            //}
+
+            //string[] links = File.ReadAllLines(gameLinksPath).Where(line => !line.StartsWith(";", StringComparison.InvariantCulture)).ToArray();
+
+            //string lastgroup = string.Empty;
+            //var linksInfo = new Dictionary<string, List<string[]>>();
+
+            //// add info
+            //foreach (var line in links)
+            //{
+            //    var info = line.Split(new string[] { _linksSeparator }, StringSplitOptions.None);
+            //    if (info.Length != 3) continue;
+
+            //    var category = info[0];
+            //    if (!linksInfo.ContainsKey(category)) linksInfo.Add(category, new List<string[]>());
+
+            //    linksInfo[category].Add(new[] { info[1], info[2] });
+            //}
+
+            //// create html string
+            //var categoriesInfo = new List<string>();
+            //foreach (var category in linksInfo)
+            //{
+            //    var descriptonLink = new List<string>();
+            //    foreach (var link in category.Value)
+            //    {
+            //        descriptonLink.Add(ManageSettings.UpdateReport.HtmlReportCategoryItemTemplate.Replace("%link%", link[1])
+            //            .Replace("%text%", new Uri(link[1]).Host.ToUpperInvariant())
+            //            .Replace("%description%", TryGetTranslation(link[0], langID))
+            //            );
+            //    }
+
+            //    categoriesInfo.Add(ManageSettings.UpdateReport.HtmlReportCategoryTemplate.Replace("%category%", groupNames.TryGetValue(category.Key)).Replace("%items%", string.Join("<br>", descriptonLink)));
+            //}
+
+            //// create new report contet
+            //var reportMessage = File.ReadAllText(ManageSettings.UpdateReport.ReportFilePath)
+            // .Replace(ManageSettings.UpdateReport.BgImageLinkPathPattern, ManageSettings.UpdateReport.CurrentGameBgFilePath)
+            // .Replace(ManageSettings.UpdateReport.ModsUpdateReportHeaderTextPattern, T._("Links"))
+            // .Replace(ManageSettings.UpdateReport.SingleModUpdateReportsTextSectionPattern, string.Join(ManageSettings.UpdateReport.HtmlBetweenModsText, categoriesInfo) + "<br>")
+            // .Replace(ManageSettings.UpdateReport.ModsUpdateInfoNoticePattern, ManageSettings.UpdateReport.ModsUpdateInfoNoticeText);
+
+            //var htmlfile = Path.Combine(ManageSettings.CurrentGameLinksInfoDirPath, ManageSettings.CurrentGame.GameAbbreviation + ".html");
+
+            //Directory.CreateDirectory(Path.GetDirectoryName(htmlfile));// fix missing parent directory error
+
+            //File.WriteAllText(htmlfile, reportMessage);
+            //using (var process = new System.Diagnostics.Process())
+            //{
+            //    try
+            //    {
+            //        process.StartInfo.UseShellExecute = true;
+            //        process.StartInfo.FileName = htmlfile;
+            //        process.Start();
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+            //}
+            #endregion old
+        }
+
+        private static string RestoreString(LinksInfos iniInfos, string s, string keyName, string baseRecordID)
+        {
+            if (!s.Contains("%")) return s;
+
+            var matches = Regex.Matches(s, @"\%[^\%]+\%");
+            var retS = s;
+            foreach (Match match in matches)
             {
-                var en = Regex.Match(line, ";##([^<]+)");
-                if (!en.Success) continue;
+                if (string.Equals(match.Value, "%base%", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    string s1 = ReplaceBase(iniInfos, s, keyName, baseRecordID);
+                    if (s1 == null) continue;
 
-                var t = Regex.Match(line, langID + "([^<]+)");
-                if (!t.Success) continue;
-                if (groupNames.ContainsKey(en.Groups[1].Value)) continue;
+                    retS = s1;
+                }
+                else
+                {
 
-                groupNames.Add(en.Groups[1].Value, t.Groups[1].Value);
+                }
             }
 
-            string[] links = File.ReadAllLines(gameLinksPath).Where(line => !line.StartsWith(";", StringComparison.InvariantCulture)).ToArray();
+            return retS;
+        }
 
-            string lastgroup = string.Empty;
-            var linksInfo = new Dictionary<string, List<string[]>>();
+        // chack if have base marker
+        // check base for empty
+        // check if have key
+        // get key value
 
-            // add info
-            foreach (var line in links)
+        private static string ReplaceBase(LinksInfos iniInfos, string s, string keyName, string baseRecordID)
+        {
+            if (string.IsNullOrEmpty(baseRecordID)) return null;
+            if (!iniInfos.ContainsKey(baseRecordID)) return null;
+
+            var baseRecodrKeys = iniInfos[baseRecordID];
+
+            // try get key value from baseData
+            if (baseRecodrKeys.ContainsKey(keyName))
             {
-                var info = line.Split(new string[] { _linksSeparator }, StringSplitOptions.None);
-                if (info.Length != 3) continue;
+                var baseKey = baseRecodrKeys[keyName];
 
-                var category = info[0];
-                if (!linksInfo.ContainsKey(category)) linksInfo.Add(category, new List<string[]>());
-
-                linksInfo[category].Add(new[] { info[1], info[2] });
+                return s.Replace("%base%", baseKey);
             }
-
-            // create html string
-            var categoriesInfo = new List<string>();
-            foreach (var category in linksInfo)
+            else // have no key!
             {
-                var descriptonLink = new List<string>();
-                foreach (var link in category.Value)
-                {
-                    descriptonLink.Add(ManageSettings.UpdateReport.HtmlReportCategoryItemTemplate.Replace("%link%", link[1])
-                        .Replace("%text%", new Uri(link[1]).Host.ToUpperInvariant())
-                        .Replace("%description%", TryGetTranslation(link[0], langID))
-                        );
-                }
+                // read possible base from parent
+                if (!baseRecodrKeys.ContainsKey("base")) return null;
+                var baseSub = baseRecodrKeys["base"];
 
-                categoriesInfo.Add(ManageSettings.UpdateReport.HtmlReportCategoryTemplate.Replace("%category%", groupNames.TryGetValue(category.Key)).Replace("%items%", string.Join("<br>", descriptonLink)));
-            }
+                var s1 = ReplaceBase(iniInfos, s, keyName, baseSub);
+                if (s1 == null) return null;
 
-            // create new report contet
-            var reportMessage = File.ReadAllText(ManageSettings.UpdateReport.ReportFilePath)
-             .Replace(ManageSettings.UpdateReport.BgImageLinkPathPattern, ManageSettings.UpdateReport.CurrentGameBgFilePath)
-             .Replace(ManageSettings.UpdateReport.ModsUpdateReportHeaderTextPattern, T._("Links"))
-             .Replace(ManageSettings.UpdateReport.SingleModUpdateReportsTextSectionPattern, string.Join(ManageSettings.UpdateReport.HtmlBetweenModsText, categoriesInfo) + "<br>")
-             .Replace(ManageSettings.UpdateReport.ModsUpdateInfoNoticePattern, ManageSettings.UpdateReport.ModsUpdateInfoNoticeText);
-
-            var htmlfile = Path.Combine(ManageSettings.CurrentGameLinksInfoDirPath, ManageSettings.CurrentGame.GameAbbreviation + ".html");
-
-            Directory.CreateDirectory(Path.GetDirectoryName(htmlfile));// fix missing parent directory error
-
-            File.WriteAllText(htmlfile, reportMessage);
-            using (var process = new System.Diagnostics.Process())
-            {
-                try
-                {
-                    process.StartInfo.UseShellExecute = true;
-                    process.StartInfo.FileName = htmlfile;
-                    process.Start();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
+                return s1;
             }
         }
 

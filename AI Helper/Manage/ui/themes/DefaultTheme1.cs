d6b559f210e2144c0685de858ee5a5d5bec1d8ff
TokcDK <TokcDK@gmail.com>
2022-12-05 20:23:40 +0500

    some test changes to default theme

--- AI Helper/Manage/ui/themes/DefaultTheme1.cs
+++ AI Helper/Manage/ui/themes/DefaultTheme1.cs
@@ -8,24 +8,25 @@ namespace AIHelper.Manage.ui.themes
     internal class DefaultTheme1 : IUITheme
     {
         public string Name => "Default";
+        Color _bg = Color.FromArgb(255, 42, 57, 80);
 
         public List<ElementData> Elements => new List<ElementData>()
         {
             new ElementData()
             {
                 Type = typeof(Panel),
-                BackColor = Color.Gray,
+                BackColor = _bg,
             },
             new ElementData()
             {
                 Type = typeof(Button),
-                BackColor = Color.Silver,
-                ForeColor = Color.Black,
+                BackColor = Color.FromArgb(255, 115, 96, 124),
+                ForeColor = Color.White,
             },
             new ElementData()
             {
                 Type = typeof(TabControl),
-                BackColor = Color.Gray,
+                BackColor = _bg,
                 ForeColor = Color.Black,
             },
         };

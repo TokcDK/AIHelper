351cd7547ca387bc5cb464f9c0a6e132a2f7e07c
TokcDK <TokcDK@gmail.com>
2023-01-05 12:16:11 +0500

    close main form before exit app

--- AI Helper/Forms/MainForm.cs
+++ AI Helper/Forms/MainForm.cs
@@ -94,6 +94,7 @@ namespace AIHelper
                         }
                     }
 
+                    this.Close();
                     Application.Exit();
                     return;
                 }

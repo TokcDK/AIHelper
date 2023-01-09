4958bb291a6f168ab5ab2c269a0603cacfd5b3af
TokcDK <TokcDK@gmail.com>
2023-01-09 11:21:28 +0500

    ..

--- AI Helper/Manage/ManageModOrganizer.cs
+++ AI Helper/Manage/ManageModOrganizer.cs
@@ -3018,21 +3018,6 @@ namespace AIHelper.Manage
                 ? modNameToInsertWith 
                 : "";
             modList.Insert(record, modName, placeAfter);
-
-            //if (modname.Length > 0)
-            //{
-            //    string currentMOprofile = ManageSettings.GetMoSelectedProfileDirName();
-
-            //    if (currentMOprofile.Length == 0)
-            //    {
-            //    }
-            //    else
-            //    {
-            //        string profilemodlistpath = ManageSettings.GetCurrentMoProfileModlistPath();
-
-            //        InsertLineInModlistFile(profilemodlistpath, (activate ? "+" : "-") + modname, 1, recordWithWhichInsert, placeAfter);
-            //    }
-            //}
         }
 
         //https://social.msdn.microsoft.com/Forums/vstudio/en-US/8f713e50-0789-4bf6-865f-c87cdebd0b4f/insert-line-to-text-file-using-streamwriter-using-csharp?forum=csharpgeneral
@@ -3044,60 +3029,62 @@ namespace AIHelper.Manage
         /// <param name="position"></param>
         public static void InsertLineInModlistFile(string moModlistPath, string line, int position = 1, string insertWithThisMod = "", bool placeAfter = true)
         {
-            if (moModlistPath.Length > 0 && File.Exists(moModlistPath) && line.Length > 0)
+            if (moModlistPath.Length == 0 || !File.Exists(moModlistPath) || line.Length == 0)
             {
-                string[] fileLines = File.ReadAllLines(moModlistPath);
-                bool isEnabled;
-                if (!(isEnabled = fileLines.Contains("+" + line.Remove(0, 1))) && !fileLines.Contains("-" + line.Remove(0, 1)))
-                {
-                    int fileLinesLength = fileLines.Length;
-                    bool insertWithMod = insertWithThisMod.Length > 0;
-                    position = insertWithMod ? fileLinesLength : position;
-                    using (StreamWriter writer = new StreamWriter(moModlistPath))
-                    {
-                        for (int lineNumber = 0; lineNumber < position; lineNumber++)
-                        {
-                            if (insertWithMod && fileLines[lineNumber].Length > 0 && string.Compare(fileLines[lineNumber].Remove(0, 1), insertWithThisMod, true, CultureInfo.InvariantCulture) == 0)
-                            {
-                                if (placeAfter)
-                                {
-                                    position = lineNumber;
-                                }
-                                else
-                                {
-                                    writer.WriteLine(fileLines[lineNumber]);
-                                    position = lineNumber + 1;
-                                }
-                                break;
+                return;
+            }
 
+            string[] fileLines = File.ReadAllLines(moModlistPath);
+            bool isEnabled;
+            if (!(isEnabled = fileLines.Contains("+" + line.Remove(0, 1))) && !fileLines.Contains("-" + line.Remove(0, 1)))
+            {
+                int fileLinesLength = fileLines.Length;
+                bool insertWithMod = insertWithThisMod.Length > 0;
+                position = insertWithMod ? fileLinesLength : position;
+                using (StreamWriter writer = new StreamWriter(moModlistPath))
+                {
+                    for (int lineNumber = 0; lineNumber < position; lineNumber++)
+                    {
+                        if (insertWithMod && fileLines[lineNumber].Length > 0 && string.Compare(fileLines[lineNumber].Remove(0, 1), insertWithThisMod, true, CultureInfo.InvariantCulture) == 0)
+                        {
+                            if (placeAfter)
+                            {
+                                position = lineNumber;
                             }
                             else
                             {
                                 writer.WriteLine(fileLines[lineNumber]);
+                                position = lineNumber + 1;
                             }
+                            break;
+
                         }
-
-                        writer.WriteLine(line);
-
-                        if (position < fileLinesLength)
+                        else
                         {
-                            for (int lineNumber = position; lineNumber < fileLinesLength; lineNumber++)
-                            {
-                                writer.WriteLine(fileLines[lineNumber]);
-                            }
+                            writer.WriteLine(fileLines[lineNumber]);
+                        }
+                    }
+
+                    writer.WriteLine(line);
+
+                    if (position < fileLinesLength)
+                    {
+                        for (int lineNumber = position; lineNumber < fileLinesLength; lineNumber++)
+                        {
+                            writer.WriteLine(fileLines[lineNumber]);
                         }
                     }
                 }
-                else
+            }
+            else
+            {
+                var prefix = "-";
+                if (isEnabled)
                 {
-                    var prefix = "-";
-                    if (isEnabled)
-                    {
-                        prefix = "+";
-                    }
-                    fileLines = fileLines.Replace(prefix + line.Remove(0, 1), line);
-                    File.WriteAllLines(moModlistPath, fileLines);
+                    prefix = "+";
                 }
+                fileLines = fileLines.Replace(prefix + line.Remove(0, 1), line);
+                File.WriteAllLines(moModlistPath, fileLines);
             }
         }
 

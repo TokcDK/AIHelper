1b8ada254d34ba73d151c8512d9759f34d56f307
TokcDK <TokcDK@gmail.com>
2023-02-26 15:08:22 +0500

    ver up

--- AI Helper/Properties/AssemblyInfo.cs
+++ AI Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.1.5.1")]
-[assembly: AssemblyFileVersion("23.1.5.1")]
+[assembly: AssemblyVersion("23.2.26.1")]
+[assembly: AssemblyFileVersion("23.2.26.1")]
 [assembly: NeutralResourcesLanguage("en")]

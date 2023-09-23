1d9ebdabd0587faed69b10d82a58889b600330b9
TokcDK <TokcDK@gmail.com>
2023-09-23 22:30:30 +0500

    paths update ver up

--- AI Helper/Properties/AssemblyInfo.cs
+++ AI Helper/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.9.2.1")]
-[assembly: AssemblyFileVersion("23.9.2.1")]
+[assembly: AssemblyVersion("23.9.23.1")]
+[assembly: AssemblyFileVersion("23.9.23.1")]
 [assembly: NeutralResourcesLanguage("en")]
